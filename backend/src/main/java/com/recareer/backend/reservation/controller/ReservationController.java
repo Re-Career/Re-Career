b25cb28b7d2495815a9d25b5b7ee0582ae682c2b
Yeshin Lee <yeslee5004@gmail.com>
2025-07-30 22:35:31 +0900

    :sparkles: Feat: reservationRequestDto 추가

--- backend/src/main/java/com/recareer/backend/reservation/controller/ReservationController.java
+++ backend/src/main/java/com/recareer/backend/reservation/controller/ReservationController.java
@@ -1,8 +1,12 @@
 package com.recareer.backend.reservation.controller;
 
+import com.recareer.backend.reservation.dto.ReservationRequestDto;
 import com.recareer.backend.reservation.entity.Reservation;
 import com.recareer.backend.reservation.service.ReservationService;
 import com.recareer.backend.response.ApiResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -11,11 +15,13 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/reservations")
 @RequiredArgsConstructor
+@Tag(name = "Reservation", description = "예약 관련 API")
 public class ReservationController {
 
   private final ReservationService reservationService;
 
   @GetMapping("/user/{userId}")
+  @Operation(summary = "유저 별 멘토링 예약 리스트 조회")
   public ResponseEntity<ApiResponse<List<Reservation>>> getReservationsByUserId(@PathVariable Long userId) {
     List<Reservation> reservations = reservationService.findAllReservationsByUserId(userId);
 
@@ -23,9 +29,10 @@ public class ReservationController {
   }
 
   @PostMapping
-  public ResponseEntity<ApiResponse<String>> createReservation(@RequestBody Reservation reservation) {
-    reservationService.createReservation(reservation);
+  @Operation(summary = "멘토링 예약 생성")
+  public ResponseEntity<ApiResponse<Long>> createReservation(@Valid @RequestBody ReservationRequestDto requestDto) {
+    Long newReservationId = reservationService.createReservation(requestDto);
 
-    return ResponseEntity.ok(ApiResponse.success("예약이 완료되었습니다."));
+    return ResponseEntity.ok(ApiResponse.success(newReservationId));
   }
 }
