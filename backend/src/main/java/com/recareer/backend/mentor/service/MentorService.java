d869d0d61ba8c49ba09448c8b898880e700871eb
Yeshin Lee <yeslee5004@gmail.com>
2025-08-07 17:42:09 +0900

    ✨Feat: Mentor 모듈 추가: Repository, Controller, Service

---
+++ backend/src/main/java/com/recareer/backend/mentor/service/MentorService.java
@@ -0,0 +1,26 @@
+package com.recareer.backend.mentor.service;
+
+import com.recareer.backend.availableTime.entity.AvailableTime;
+import com.recareer.backend.mentor.entity.Mentor;
+import com.recareer.backend.reservation.entity.Reservation;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+public interface MentorService {
+    
+    Optional<Mentor> getVerifiedMentorById(Long id);
+
+    List<Mentor> getVerifiedMentors();
+    
+    List<Mentor> getMentorsByRegion(String region);
+    
+    Optional<Mentor> updateMentor(Long id, String position, String description);
+    
+    List<Reservation> getMentorReservations(Long mentorId);
+
+    List<AvailableTime> getMentorAvailableTimes(Long mentorId);
+    
+    AvailableTime createMentorAvailableTime(Long mentorId, LocalDateTime availableTime);
+}
