41dddd4829962db79ce297356e90042d9d283f23
Yeshin Lee <yeslee5004@gmail.com>
2025-08-07 17:42:40 +0900

    ✨Feat: AvailableTimeRepository, ReservationRepository 수정

--- backend/src/main/java/com/recareer/backend/availableTime/repository/AvailableTimeRepository.java
+++ backend/src/main/java/com/recareer/backend/availableTime/repository/AvailableTimeRepository.java
@@ -1,7 +1,12 @@
 package com.recareer.backend.availableTime.repository;
 
 import com.recareer.backend.availableTime.entity.AvailableTime;
+import com.recareer.backend.mentor.entity.Mentor;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface AvailableTimeRepository extends JpaRepository<AvailableTime, Long> {
+    
+    List<AvailableTime> findByMentor(Mentor mentor);
 }
