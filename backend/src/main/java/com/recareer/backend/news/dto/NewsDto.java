b8c78273bd33ab73ffbec112a0937192a5bd2dd7
ayeonChoi <ayeon3590@gmail.com>
2025-08-12 17:25:52 +0900

    뉴스 api 추가

---
+++ backend/src/main/java/com/recareer/backend/news/dto/NewsDto.java
@@ -0,0 +1,16 @@
+package com.recareer.backend.news.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class NewsDto {
+    private Long id;
+    private String title;
+    private String description;
+    private String url;
+    private String category;
+}
