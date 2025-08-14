3380c5aae8f79783cef2c4eb6b0e2ca5008c2d23
Yeshin Lee <yeslee5004@gmail.com>
2025-08-14 16:46:04 +0900

    Revert ":white_check_mark: Test: ApplicationTest가 dev 환경에서만 실행되도록 수정"
    This reverts commit 13ed4416ba22123536ff14cc4766d04ecb845b7b.

--- backend/src/test/java/com/recareer/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/recareer/backend/BackendApplicationTests.java
@@ -2,10 +2,8 @@ package com.recareer.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-@ActiveProfiles("dev")
 class BackendApplicationTests {
 
 	@Test
