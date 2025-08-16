2e01d19a6eb10d71b8482c8fdcc9d0fefd68250a
weniger2021 <weniger2021@gmail.com>
2025-08-16 15:10:56 +0900

    fix : build 에러 개선

--- frontend/career-web/src/mocks/home/mentor-list.ts
+++ frontend/career-web/src/mocks/home/mentor-list.ts
@@ -5,6 +5,7 @@ export const mentors: Mentor[] = [
     id: 1,
     name: '김지연',
     job: '시니어 소프트웨어 엔지니어',
+    email: '',
     profileImage:
       'https://images.unsplash.com/photo-1559839734-2b71ea197ec2?w=120&h=120&fit=crop&crop=face',
   },
@@ -12,6 +13,7 @@ export const mentors: Mentor[] = [
     id: 2,
     name: '박서영',
     job: '프로덕트 매니저',
+    email: '',
     profileImage:
       'https://images.unsplash.com/photo-1573496359142-b8d87734a5a2?w=120&h=120&fit=crop&crop=face',
   },
@@ -19,6 +21,7 @@ export const mentors: Mentor[] = [
     id: 3,
     name: '이서연',
     job: 'UX/UI 디자이너',
+    email: '',
     profileImage:
       'https://images.unsplash.com/photo-1438761681033-6461ffad8d80?w=120&h=120&fit=crop&crop=face',
   },
@@ -26,6 +29,7 @@ export const mentors: Mentor[] = [
     id: 4,
     name: '정수민',
     job: '데이터 사이언티스트',
+    email: '',
     profileImage:
       'https://images.unsplash.com/photo-1580489944761-15a19d654956?w=120&h=120&fit=crop&crop=face',
   },
@@ -33,6 +37,7 @@ export const mentors: Mentor[] = [
     id: 5,
     name: '최예린',
     job: '마케팅 매니저',
+    email: '',
     profileImage:
       'https://images.unsplash.com/photo-1544005313-94ddf0286df2?w=120&h=120&fit=crop&crop=face',
   },
