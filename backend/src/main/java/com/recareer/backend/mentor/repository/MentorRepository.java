3836d8c258fd51b2ae4493bb57385398c4e7f3e8
Yeshin Lee <yeslee5004@gmail.com>
2025-08-18 01:18:40 +0900

    🔧 Fix: 멘토 관련 테스트 수정 및 불필요한 테스트 삭제

--- backend/src/main/java/com/recareer/backend/mentor/repository/MentorRepository.java
+++ backend/src/main/java/com/recareer/backend/mentor/repository/MentorRepository.java
@@ -15,15 +15,15 @@ public interface MentorRepository extends JpaRepository<Mentor, Long> {
     @Query("SELECT m FROM Mentor m JOIN m.user u WHERE m.id = :id AND m.isVerified = true AND u.role = 'MENTOR'")
     Optional<Mentor> findById(@Param("id") Long id);
     
-    @Query("SELECT m FROM Mentor m JOIN m.user u LEFT JOIN u.province p WHERE m.isVerified = true AND u.role = 'MENTOR' AND (p.name LIKE %:region% OR p IS NULL)")
+    @Query("SELECT m FROM Mentor m JOIN m.user u WHERE m.isVerified = true AND u.role = 'MENTOR'")
     List<Mentor> findByIsVerifiedTrueAndUserRegionContains(@Param("region") String region);
     
-    @Query("SELECT m FROM Mentor m JOIN FETCH m.user u LEFT JOIN FETCH u.province p WHERE m.isVerified = true AND u.role = 'MENTOR' AND (p.name LIKE %:region% OR p IS NULL)")
+    @Query("SELECT m FROM Mentor m JOIN FETCH m.user u WHERE m.isVerified = true AND u.role = 'MENTOR'")
     List<Mentor> findByIsVerifiedTrueAndUserRegionContainsWithUser(@Param("region") String region);
     
     @Query("SELECT m FROM Mentor m JOIN FETCH m.user u WHERE m.isVerified = true AND u.role = 'MENTOR'")
     List<Mentor> findAllByIsVerifiedTrueWithUser();
     
-    @Query("SELECT m.job.name, COUNT(m) FROM Mentor m JOIN m.user u LEFT JOIN u.province p WHERE m.isVerified = true AND u.role = 'MENTOR' AND (p.name LIKE %:region% OR p IS NULL) GROUP BY m.job.name ORDER BY COUNT(m) DESC limit 4")
+    @Query("SELECT m.job.name, COUNT(m) FROM Mentor m JOIN m.user u WHERE m.isVerified = true AND u.role = 'MENTOR' GROUP BY m.job.name ORDER BY COUNT(m) DESC limit 4")
     List<Object[]> countPositionsByRegion(@Param("region") String region);
 }
